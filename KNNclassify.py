010f60a5b3d082e58843f4576b6797a92342520e
Indian-otaku <zurajanaikatsurada1222@gmail.com>
2022-12-27 20:21:30 +0530

    Hide some files using .gitignore

--- KNNclassify.py
+++ KNNclassify.py
@@ -1,13 +1,10 @@
+import random
 import numpy as np
 import warnings
 import pandas as pd
-import matplotlib.pyplot as plt
-from matplotlib import style
 from collections import Counter
 
-style.use('fivethirtyeight')
-
-data = {'r':[[1,2],[2,4],[1,3],[1,4]],'b':[[6,7],[8,6],[7,9],[5,8]]}
+# Branch to test out whether the KNN classifier works well using real data and compared it with sklearn kNN classifier
 
 def KNNclassifier(data, predict, k=3):
     if (len(data) >= k):
@@ -24,8 +21,34 @@ def KNNclassifier(data, predict, k=3):
 
     return vote_result
 
-[[plt.scatter(ii[0],ii[1],s=100,c=i) for ii in data[i]] for i in data]
-predict = [3,5]
-predict_class = KNNclassifier(data,predict,k=3)
-plt.scatter(*predict,s=200,c=predict_class)
-plt.show()
+accuracies = []
+for i in range(20):
+    df = pd.read_csv('breast-cancer-wisconsin.data',na_values='?')
+    df.replace(np.nan,-99999,inplace=True)
+    df.drop(['Sample_code_number'],axis='columns',inplace=True)
+
+    data_list = df.astype(float).values.tolist()
+    random.shuffle(data_list)
+
+    test_size = 0.2
+    train_set = {2:[],4:[]}
+    test_set = {2:[],4:[]}
+    train_data = data_list[:-int(len(data_list)*test_size)]
+    test_data = data_list[-int(len(data_list)*test_size):]
+
+    [train_set[i[-1]].append(i[:-1]) for i in train_data]
+    [test_set[i[-1]].append(i[:-1]) for i in test_data]
+
+    correct = 0
+    total = 0
+    for group in test_set:
+        for feature in test_set[group]:
+            prediction = KNNclassifier(train_set,feature,k=5)
+            if (group == prediction):
+                correct += 1
+            total += 1
+
+    # print("Accuracy: ",correct/total)
+    accuracies.append(correct/total)
+
+print("Average accuracy: ",sum(accuracies)/len(accuracies))
