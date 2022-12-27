e1b489cb885effbffbbcb44ead6cca54f0e508b7
Indian-otaku <zurajanaikatsurada1222@gmail.com>
2022-12-27 16:45:41 +0530

    Created KNNprogram.py

--- KNNclassify.py
+++ KNNclassify.py
@@ -21,29 +21,34 @@ def KNNclassifier(data, predict, k=3):
 
     return vote_result
 
-df = pd.read_csv('breast-cancer-wisconsin.data',na_values='?')
-df.replace(np.nan,-99999,inplace=True)
-df.drop(['Sample_code_number'],axis='columns',inplace=True)
+accuracies = []
+for i in range(20):
+    df = pd.read_csv('breast-cancer-wisconsin.data',na_values='?')
+    df.replace(np.nan,-99999,inplace=True)
+    df.drop(['Sample_code_number'],axis='columns',inplace=True)
 
-data_list = df.astype(float).values.tolist()
-random.shuffle(data_list)
+    data_list = df.astype(float).values.tolist()
+    random.shuffle(data_list)
 
-test_size = 0.2
-train_set = {2:[],4:[]}
-test_set = {2:[],4:[]}
-train_data = data_list[:-int(len(data_list)*test_size)]
-test_data = data_list[-int(len(data_list)*test_size):]
+    test_size = 0.2
+    train_set = {2:[],4:[]}
+    test_set = {2:[],4:[]}
+    train_data = data_list[:-int(len(data_list)*test_size)]
+    test_data = data_list[-int(len(data_list)*test_size):]
 
-[train_set[i[-1]].append(i[:-1]) for i in train_data]
-[test_set[i[-1]].append(i[:-1]) for i in test_data]
+    [train_set[i[-1]].append(i[:-1]) for i in train_data]
+    [test_set[i[-1]].append(i[:-1]) for i in test_data]
 
-correct = 0
-total = 0
-for group in test_set:
-    for feature in test_set[group]:
-        prediction = KNNclassifier(train_set,feature,k=5)
-        if (group == prediction):
-            correct += 1
-        total += 1
+    correct = 0
+    total = 0
+    for group in test_set:
+        for feature in test_set[group]:
+            prediction = KNNclassifier(train_set,feature,k=5)
+            if (group == prediction):
+                correct += 1
+            total += 1
 
-print("Accuracy: ",correct/total)
+    # print("Accuracy: ",correct/total)
+    accuracies.append(correct/total)
+
+print("Average accuracy: ",sum(accuracies)/len(accuracies))
